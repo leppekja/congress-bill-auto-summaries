8e925fcb25b08f149b71c5cdf6bfcdc5916244d5
leppekj <leppekj@uchicago.edu>
2021-04-12 01:36:52 -0500

    updates with a complete summary data set

--- data_collection.py
+++ data_collection.py
@@ -2,10 +2,11 @@ import requests
 from requests.exceptions import HTTPError
 import json
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 API_SUMMARIES_INDEX_URL = "https://www.govinfo.gov/bulkdata/json/BILLSUM/"
 # FOLDER_NAMES = [str(i) for i in range(113, 118)]
-FOLDER_NAMES = ['117']
+FOLDER_NAMES = list(range(113, 118))
 BILL_TYPE_IDS = ['hr', 's']
 
 
@@ -20,7 +21,8 @@ def collect_summaries():
     bill_count = 0
 
     try:
-        for url in [API_SUMMARIES_INDEX_URL + i + '/' for i in FOLDER_NAMES]:
+        for url in [API_SUMMARIES_INDEX_URL + str(i) + '/' for i in FOLDER_NAMES]:
+            print("Collecting bill summaries from the " + str(i) + "th Congress")
             for bill_type in BILL_TYPE_IDS:
                 summaries_list = http_call(url + bill_type)
 
@@ -34,7 +36,7 @@ def collect_summaries():
                                 xml_summary
                             ))
                     bill_count += 1
-                    if bill_count % 10 == 0:
+                    if bill_count % 100 == 0:
                         print(bill_count, "Bills Counted")
 
     except HTTPError as e:
@@ -43,7 +45,42 @@ def collect_summaries():
     return bill_summaries
 
 
+def collect_bills_info():
+    """
+    Collects the names of the files for each bill, as well as a link to
+    the text. The summaries don't contain information based on the session
+    of Congress that the bill was introduced in. However, the session is 
+    used to construct the url that the text of the bill is stored at. Given this,
+    we can't construct the URL programatically from the measure ID we collect
+    from the summary, unless Congress only had one session (this is only the case
+    with the current Congress). As a workaround, we obtain the meta-information
+    for each file. We parse the measure ID from each bill, and return a 
+    dataframe with the measure ID and the link to the text of the bill. 
+    """
+
+    return None
+
+
+def join_summaries_to_bills(summaries, bills, summaries_col, bills_col):
+    """
+    Joins the summaries to the link of the bill text based on the measure ID.
+    Returns Pandas DataFrame.
+    """
+    return None
+
+
+def collect_bill_text(urls):
+    """
+    Given urls of the bill text, programmatically collects the text from the 
+    Govinfo Bulk Data Repository.
+    """
+    return None
+
+
 def parse_xml_summary(xml):
+    """
+    Helper function to parse XML for bill summaries.
+    """
     root = ET.fromstring(xml)
     try:
         measure_id = root[0].attrib['measure-id']
@@ -70,3 +107,9 @@ def http_call(url, return_text=False):
             return r.json()
     else:
         raise HTTPError
+
+
+if __name__ == "__main__":
+    bill_summaries = collect_summaries()
+    df = pd.DataFrame(bill_summaries)
+    df.to_csv("Summaries.csv", index=False)
---
+++ preprocess-summaries.py
@@ -0,0 +1,23 @@
+import re
+
+
+def remove_html_tags(summary):
+    return re.sub('<.+?>', '', summary)
+
+
+def lowercase(summary):
+    return summary.lower()
+
+
+def replace_digits(summary):
+    '''
+    Should be run post-remove_punctuation.
+    '''
+    return re.sub('\d', '#', summary)
+
+
+def remove_punctuation(summary):
+    '''
+    Note remove_punctuation needs to be run prior to digit replacement.
+    '''
+    return re.sub('\W', '', summary)
