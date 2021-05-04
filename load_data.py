914079215b8f9812c97cc2fa18cba97cd21f963d
leppekj <leppekj@uchicago.edu>
2021-05-04 18:41:43 -0500

    fixed dataloader to return the index of the word

--- load_data.py
+++ load_data.py
@@ -10,9 +10,10 @@ from collections import Counter
 from functools import partial
 
 
-def build_vocab(training_data, summary_col, bill_col):
+def build_vocab(training_data, summary_col='summary', bill_col='bill'):
     '''
-    Builds a Vocab object for a Dataset object. 
+    Builds a Vocab object for a Dataset object.
+    If default BillsDataset object, summary and bill are dict keys.
     '''
     # Here I used a single counter for both bills and summaries
     # Since we don't want to limit the vocab
@@ -53,7 +54,7 @@ def split(data, training_size, testing_size, valid_size, shuffle_data=True):
             BillsDataset(validate_data, 'summary_clean', 'bill_clean'))
 
 
-def get_dataloaders(batch_size, max_summary_length, max_bill_length, **kwargs):
+def get_dataloaders(batch_size, vocab, max_summary_length, max_bill_length, **kwargs):
     '''
     kwargs for training_data:, test_data:, and validation_data:.
     Returns dict of dataloaders based on arg name input
@@ -62,7 +63,7 @@ def get_dataloaders(batch_size, max_summary_length, max_bill_length, **kwargs):
     # Set params for the collate function
     # uses the max length of any bill and summary across the entire data set
     collate_fn = partial(
-        collate_bills_fn, max_summary_length=max_summary_length, max_bill_length=max_bill_length)
+        collate_bills_fn, vocab=vocab, max_summary_length=max_summary_length, max_bill_length=max_bill_length)
 
     for dataset_name, data in kwargs.items():
         dataloaders[dataset_name] = DataLoader(
@@ -72,7 +73,7 @@ def get_dataloaders(batch_size, max_summary_length, max_bill_length, **kwargs):
     return dataloaders
 
 
-def collate_bills_fn(batch, max_summary_length=512, max_bill_length=2048):
+def collate_bills_fn(batch, vocab, max_summary_length=512, max_bill_length=2048):
     '''
     Collates the batches into the dataloader. Pads unequal lengths with zeros
     based on the max lengths given.
@@ -88,21 +89,29 @@ def collate_bills_fn(batch, max_summary_length=512, max_bill_length=2048):
         label = text_dict['summary']
         text = text_dict['bill']
         # Translate to GloVe embeddings
-        label_vectors = glove.get_vecs_by_tokens(label)
-        text_vectors = glove.get_vecs_by_tokens(text)
+        # label_vectors = glove.get_vecs_by_tokens(label)
+        # text_vectors = glove.get_vecs_by_tokens(text)
+        label_vectors = []
+        text_vectors = []
+        for word in label:
+            label_vectors.append(vocab.stoi[word])
+        for word in text:
+            text_vectors.append(vocab.stoi[word])
         # Check lengths; see how many zero rows to pad
-        label_length = label_vectors.shape[0]
-        text_length = text_vectors.shape[0]
+        label_length = len(label_vectors)
+        text_length = len(text_vectors)
         labels_to_pad = max_summary_length - label_length
         text_to_pad = max_bill_length - text_length
 
         if label_length < max_summary_length:
-            label_vectors = F.pad(label_vectors, (0, 0, 0, labels_to_pad))
+            label_vectors.extend([0] * labels_to_pad)
+            # label_vectors = F.pad(label_vectors, (0, 0, 0, labels_to_pad))
         if text_length < max_bill_length:
-            text_vectors = F.pad(text_vectors, (0, 0, 0, text_to_pad))
+            text_vectors.extend([0] * text_to_pad)
+            # text_vectors = F.pad(text_vectors, (0, 0, 0, text_to_pad))
 
-        labels.append(label_vectors)
-        texts.append(text_vectors)
+        labels.append(torch.Tensor(label_vectors))
+        texts.append(torch.Tensor(text_vectors))
     # Returns shape of (batch size, max_summary_length, embedding length) for each
     return (torch.stack(labels), torch.stack(texts))
 
